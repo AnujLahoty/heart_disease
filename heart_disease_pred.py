b89c3f3316f1a099d7baf15c1bfec9cc7d0eb0fb
Anuj M Lahoty <48029649+AnujLahoty@users.noreply.github.com>
2019-08-23 21:37:17 +0530

    Add files via upload

---
+++ heart_disease_pred.py
@@ -0,0 +1,50 @@
+# Logistic Regression
+# Importing the libraries
+
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('framingham.csv')
+ 
+# VISUALISING THE DATASET   
+dataset.info()
+
+
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 15].values
+
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(strategy = 'mean')
+imputer.fit(X)
+X = imputer.transform(X)
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Assesing the Model's prediction
+from sklearn.metrics import accuracy_score 
+print ('Accuracy Score :',accuracy_score(y_test, y_pred))
